1bb0dc441e3692bafbd320b6b11c47a95714cb31
Chromacry <Chromacry101@gmail.com>
2023-05-28 19:43:20 +0800

    fixed design constant file naming

--- assets/app_config.dart
+++ assets/app_config.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hydrosense_mobile_app/src/constants/DesignContants.dart';
+import 'package:hydrosense_mobile_app/src/constants/design_constants.dart';
 
 class AppConfig {
   static ThemeData themeConfig = ThemeData(
