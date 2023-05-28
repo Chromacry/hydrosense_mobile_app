637b979117e3177005b806f782ad6a188648259f
Chromacry <Chromacry101@gmail.com>
2023-05-28 17:44:58 +0800

    added theme in appconfig

--- assets/app_config.dart
+++ assets/app_config.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hydrosense_mobile_app/src/constants/DesignContants.dart';
 
 class AppConfig {
   static ThemeData themeConfig = ThemeData(
     fontFamily: 'Roboto',
+    inputDecorationTheme: const InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(width: 3, 
+        color: Colors.white,
+        ),
+      ),
+    ),
     colorScheme: ColorScheme.fromSwatch(
-      primarySwatch: Colors.blue,
+      primarySwatch: const Color(DesignConstants.COLOR_DARKBLUE_PRIMARY) as MaterialColor,
+      accentColor: Colors.white,
     ).copyWith(
       secondary: Colors.black,
     ),
