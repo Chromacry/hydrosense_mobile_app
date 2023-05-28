6229727ff69fc63c470e7312862105af5bcd32ff
Chromacry <Chromacry101@gmail.com>
2023-05-28 17:46:20 +0800

    tidied code and added new colors for design constants

--- lib/src/constants/DesignContants.dart
+++ lib/src/constants/DesignContants.dart
@@ -1,4 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
 class DesignConstants {
-  static const double FONT_SIZE_22 = 22;
+  //* Application Level
   static const debugBanner = false;
+
+  //* Font Settings
+  static const double FONT_SIZE_22 = 22;
+
+  //* For custom colors add 0xFF at the front of hex code
+  static const int COLOR_DARKBLUE_PRIMARY = 0xFF2D2D3F; //* Background color
+  static const int COLOR_DARKGRAY_SECONDARY = 0xFF393939; //* Darkgray background color
+  static const int COLOR_LIGHTBLUE_ACCENT = 0xFF9BD4FF; //* Button
 }
