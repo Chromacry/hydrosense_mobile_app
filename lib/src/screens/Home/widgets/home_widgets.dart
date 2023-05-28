c6181e975ca55f6800d6161c99b968078fd895ff
Chromacry <Chromacry101@gmail.com>
2023-05-28 17:48:49 +0800

    removed home screen

--- lib/src/screens/Home/widgets/home_widgets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'InputTextBox.dart';
-
-class HomeWidgets {
-  static Widget inputTextBox(String textLabel) {
-    return InputTextBox(inputTextLabelValue: textLabel);
-  }
-}
