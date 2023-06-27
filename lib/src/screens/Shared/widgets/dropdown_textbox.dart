24fa80e960d698fd0912042184dc426b20265480
Chromacry <Chromacry101@gmail.com>
2023-06-28 02:28:25 +0800

    added dropdowntextbox to sharedwidgets

---
+++ lib/src/screens/Shared/widgets/dropdown_textbox.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+
+class DropdownTextBox extends StatefulWidget {
+  final List<String> options;
+  final String hintText;
+
+  DropdownTextBox({
+    super.key,
+    required this.options,
+    required this.hintText,
+  });
+
+  @override
+  State<DropdownTextBox> createState() => _DropdownTextBoxState();
+}
+
+class _DropdownTextBoxState extends State<DropdownTextBox> {
+  List<String> options = [];
+  String? selectedValue;
+
+  @override
+  void initState() {
+    super.initState();
+    options.addAll(widget.options);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 20),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2(
+          hint: Text(
+            widget.hintText,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          //* Icon of dropdown box
+          iconStyleData: IconStyleData(
+            iconEnabledColor: Colors.white,
+            icon: Icon(Icons.keyboard_arrow_down_rounded),
+          ),
+          //* Dropdown box style
+          buttonStyleData: const ButtonStyleData(
+            padding: EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              // color: Colors.grey, //* Background of dropdownbox
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.deepPurpleAccent,
+                  width: 2,
+                ),
+              ),
+            ),
+            height: 40,
+            width: 140,
+          ),
+          //* Dropdown Menu Items Styles
+          items: options
+              .map((item) => DropdownMenuItem<String>(
+                    value: item,
+                    child: Text(
+                      item,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ))
+              .toList(),
+          value: selectedValue,
+          onChanged: (value) {
+            setState(() {
+              selectedValue = value as String;
+            });
+          },
+          menuItemStyleData: const MenuItemStyleData(
+            height: 40,
+          ),
+          //* Dropdown Menu Style
+          dropdownStyleData: DropdownStyleData(
+            maxHeight: 200,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(11),
+                  bottomRight: Radius.circular(11)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/screens/Shared/widgets/shared_widgets.dart
+++ lib/src/screens/Shared/widgets/shared_widgets.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hydrosense_mobile_app/src/screens/Shared/widgets/dropdown_textbox.dart';
 import 'package:hydrosense_mobile_app/src/screens/Shared/widgets/input_textbox.dart';
 
 class SharedWidgets {
-  static Widget inputTextBox({textLabel, onChanged, obscureTextEnabled}) {
-    return InputTextBox(inputTextLabelValue: textLabel, onChanged: onChanged, obscureTextEnabled: obscureTextEnabled ?? false,);
+  static Widget inputTextBox({textLabel, allColorAttributes, onChanged, obscureTextEnabled}) {
+    return InputTextBox(inputTextLabelValue: textLabel, allColorAttributes: allColorAttributes,  onChanged: onChanged, obscureTextEnabled: obscureTextEnabled ?? false,);
   }
+  static Widget dropdownTextBox({hintText, required List<String> options}) =>
+      DropdownTextBox(
+        hintText: hintText,
+        options: options,
+      );
 }
